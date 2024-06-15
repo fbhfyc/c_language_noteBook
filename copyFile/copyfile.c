02a96f178b92d9c104749c75c3ced168bfe36f5b
fbhfyc <fbhfyc@163.com>
2024-06-15 11:04:46 +0800

    kkk

--- copyFile/copyfile.c
+++ copyFile/copyfile.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
+#include <string.h>
 
-void main()
+void main(int argc,char* argv[])
 {
+	char total[10240]={0};
+
+	if (argc != 3) {
+		printf("useg err");
+		return;
+	}
+
+ 	char buff[1024]={0};
+
+	FILE* fsrc = fopen(argv[1],"r");
+	if (fsrc == NULL) {
+		printf("file open failed");
+		return;
+	}
+
+	FILE* fdest = fopen(argv[2],"w");
+	if(fdest == NULL) {
+		printf("file open failed");
+		return;
+	}
+
+	while (fgets(buff,1024,fsrc) != NULL) {
+		strcat(total,buff);
+	}
+
+	fputs(total,fdest);
+
+	fclose(fsrc);
+        fclose(fdest);
+
 	return;
 }
